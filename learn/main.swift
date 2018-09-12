9e04d38342b53ce20ba7e9f72f57c27b2e85cd01
20161104607 <a20161104607@mac26.local>
2018-09-12 11:33:49 +0800

    1-100
    3

--- learn/main.swift
+++ learn/main.swift
@@ -8,5 +8,49 @@
 
 import Foundation
 
-print("Hello, World  learn swift!")
+var total1 = 0
+for i in 1...100{
+    total1 += i
+    }
+print("total1=\(total1)")
+
+
+
+/*
+//print("Hello, World  learn swift!")
+
+//打印
+print("hello")
+//变量和常量
+let Num = 10
+var name = "xiaoming"
+name = "lvliangyu"
+let explicitFloat:Float //定义了一个float型的变量
+explicitFloat = 70
+let explicitDouble:Double=80
+
+//值不隐拭转换 需要显示转换成其他数据类型
+
+let label = "The width is"
+let width = 94
+let widthLabel = label + String(width)
+
+// 字符串里嵌入其他变量，用反斜杠“\”
+let apples = 3
+let orangs = 5
+let myStr = "my"
+let appleSummary = "i have\(apples) apples."
+let fruitSummary = "i have\(apples + orangs) \(myStr)preces of fruit."
+
+*/
+
+
+
+
+
+
+
+
+
+
 
